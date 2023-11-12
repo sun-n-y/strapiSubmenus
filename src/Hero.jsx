c25315e8559d4c87f763ceda0312bf40f192b39e
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-12 11:18:03 -0500

    add hero strucutre and css

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,4 +1,14 @@
 const Hero = () => {
-  return <h2>Hero</h2>;
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>manage any content</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          tempore vero repudiandae ratione libero?
+        </p>
+      </div>
+    </div>
+  );
 };
 export default Hero;
