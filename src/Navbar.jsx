48c75c09c823e7c5a657b351e7abf5614f30cc69
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-12 10:53:07 -0500

    add navbar strucutre and css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,17 @@
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 const Navbar = () => {
-  return <h2>Navbar</h2>;
+  const { openSidebar } = useGlobalContext();
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
