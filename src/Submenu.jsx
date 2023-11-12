dca931bdef7ffee0a740aa11c13e727d4430c5c3
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-12 14:28:08 -0500

    add submenu structure and css

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,4 +1,32 @@
+import { useGlobalContext } from './context';
+import sublinks from './data';
+
 const Submenu = () => {
-  return <h2>Submenu</h2>;
+  const { pageId } = useGlobalContext();
+
+  const currentPage = sublinks.find((item) => pageId == item.pageId);
+
+  return (
+    <div className={currentPage ? 'submenu show-submenu' : 'submenu'}>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
