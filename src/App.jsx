b9257f8c5d9fe2e998301dd99c6aef42d092d42a
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-12 10:27:31 -0500

    add global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useGlobalContext } from './context';
+
 const App = () => {
+  const { isSidebarOpen } = useGlobalContext();
   return <h2>Strapi Starter</h2>;
 };
 export default App;
---
+++ src/context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  return (
+    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
